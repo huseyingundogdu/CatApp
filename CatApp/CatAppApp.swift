7028fb8fc6d73804940983f698de4dfbe288e761
Huseyin <benhuseyingundogdu@gmail.com>
2024-09-02 17:13:41 +0200

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by Huseyin on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
