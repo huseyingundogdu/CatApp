54c16db311bac36fb20178604f195188be67de29
Huseyin <benhuseyingundogdu@gmail.com>
2024-09-03 16:16:09 +0200

    LikedView UI created

--- CatApp/Views/LikedView.swift
+++ CatApp/Views/LikedView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct LikedView: View {
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    let items = 1...50
+    
     var body: some View {
-        Text("Liked View")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(items, id: \.self) { item in
+                        // Each item in the grid
+                        Button(action: {}, label: {
+                            RoundedRectangle(cornerRadius: 12)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 120)
+                        })
+                    }
+                }
+                .padding(.all, 10)
+                .frame(width: .infinity, height: .infinity)
+            }
+            .navigationTitle("Liked")
+        }
     }
 }
 
